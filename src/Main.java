d017ef603a63c873643f24ca81eacf06bf52304b
Sierri12 <sierri12@gmail.com>
2020-02-25 19:30:16 +0100

    ver 0.0.2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("xd");
         System.out.println("xDDD");
+        System.out.println("xDDDDD");
     }
 }
